be753af224f97525f12b93c112a8d2cd1354e88d
Azamat-JS <azamatielts987@gmail.com>
2025-03-17 08:39:44 +0500

    basic operations in python

---
+++ index.py
@@ -0,0 +1,32 @@
+# print("salom python")
+# print(int(3.1))
+# print(bool(1))
+# print(float(3))
+# name = "Azamat Abdullayev"
+# # print(name[1]) # z
+# # print(name[-2]) # e
+
+# # print(name[0:4]) # Azam
+# # print(name[9:12]) # dul
+# # print(name[::2]) # Aaa bulyv
+# # print(len(name)) #17
+# # role = " developer"
+# # print(name + role) #Azamat Abdullayev developer
+# # print(3 * name) #Azamat AbdullayevAzamat AbdullayevAzamat Abdullayev
+# A="email.com"
+# # B=A.upper()
+# # print(B) #EMAIL.COM
+# C="Bu archa"
+# D=C.replace("Bu", "Chiroyli")
+# print(D) # Chiroyli archa
+# print(C.find("rch")) #4
+# print(C.find("ge")) # -1
+# ---------------------------------------
+
+name = "John"
+age = 30
+
+print(f"His name is {name} his age is {age}")
+print("Your name is {} and your age is {}".format(name, age))
+x=2/2
+print(x)
